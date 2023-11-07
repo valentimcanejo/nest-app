a0d09b5685952f3b91ed65b979ff942d8118c28a
Rômulo Valentim <valentim.canejo@hotmail.com>
2023-11-07 07:34:15 -0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
+import { CreateAccountController } from "./controllers/create-account.controller";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
+})
+export class AppModule {}
